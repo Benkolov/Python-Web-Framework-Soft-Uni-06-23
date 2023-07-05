cf1c94dd62c52b98f6615ea79f77af7eb4067461
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-07-05 18:09:57 +0300

    Changes

--- 03.User and Password Management/auth_demos/auth_demos/app_auth/models.py
+++ 03.User and Password Management/auth_demos/auth_demos/app_auth/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+
+class AppUser(auth_models.AbstractBaseUser):
+    USERNAME_FIELD = 'email'
+
+    # objects = AppUserManager()
+
+    email = models.EmailField(
+        null=False,
+        blank=False,
+        unique=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
--- 03.User and Password Management/auth_demos/auth_demos/urls.py
+++ 03.User and Password Management/auth_demos/auth_demos/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('auth_demos.web.urls')),
     path('auth/', include('auth_demos.app_auth.urls'))
 ]
