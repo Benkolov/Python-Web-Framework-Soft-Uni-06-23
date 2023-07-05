5d98ba60a6441bed1f55916fa87c09e0393b9a67
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-07-05 13:06:20 +0300

    Initial commit

--- 03.User and Password Management/auth_demos/auth_demos/app_auth/urls.py
+++ 03.User and Password Management/auth_demos/auth_demos/app_auth/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-urlpatterns = [
+from auth_demos.app_auth.views import RegisterUserView, LoginUserView, LogoutUserView
 
+urlpatterns = [
+    path('register/', RegisterUserView.as_view(), name='register_user'),
+    path('login/', LoginUserView.as_view(), name='login_user'),
+    path('register/', LogoutUserView.as_view(), name='logout_user'),
 ]
